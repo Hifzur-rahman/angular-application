854e11f977951f04110cfb402afe63c884469006
Hifzur Rahman <hifzurrahman.h@10decoders.in>
2020-08-14 19:35:42 +0530

    Update server on heroku 3

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angular-app-crud-array'));   
+app.use(express.static(__dirname + '/angular-application'));    
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/angular-app-crud-array/index.html'));
+res.sendFile(path.join(__dirname+'/angular-application/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
